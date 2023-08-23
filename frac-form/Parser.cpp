6af5ece638ea0fb50b796d0529d5ed3575449371
Richard <legalize@xmission.com>
2023-08-22 21:04:27 -0600

    Parse real constants with a rule with Complex attribute

--- frac-form/Parser.cpp
+++ frac-form/Parser.cpp
@@ -1,5 +1,6 @@
 #include "parser.h"
 
+#include <boost/fusion/adapted.hpp>
 #include <boost/spirit/home/x3.hpp>
 
 namespace x3 = boost::spirit::x3;
@@ -8,22 +9,26 @@ namespace ascii = x3::ascii;
 namespace fractalFormula
 {
 
+auto assign = [](auto &ctx)
+{
+    _val(ctx) = Complex{_attr(ctx), 0.0};
+};
+
+struct Constant;
+x3::rule<Constant, Complex> const constant = "constant";
+auto const                        constant_def = x3::double_[assign];
+BOOST_SPIRIT_DEFINE(constant);
+
 bool parse(const std::string &text, Complex &value)
 {
+    using ascii::space;
     using x3::double_;
     using x3::phrase_parse;
-    using ascii::space;
 
-    std::string::const_iterator       begin = text.begin();
-    const std::string::const_iterator end = text.end();
-    double parsedValue;
-    const bool                        result = phrase_parse(begin, end, double_, space, parsedValue) && begin == end;
-    if(result)
-    {
-        value.re = parsedValue;
-        value.im = 0.0;
-    }
-
+    using Iterator = std::string::const_iterator;
+    Iterator       begin = text.begin();
+    const Iterator end = text.end();
+    const bool     result = phrase_parse(begin, end, constant, space, value) && begin == end;
     return result;
 }
 
